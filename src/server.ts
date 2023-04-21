60a7bdfbe3abfd1ea886c0164c61332ba0389e28
Gabriel <Kryemadhigabriel@gmail.com>
2023-04-21 18:20:25 +0200

    Ex. done

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import joi from "joi";
 
 dotenv.config();
 
@@ -23,6 +24,11 @@ let planets: Planets = [
   },
 ];
 
+const planetSchema = joi.object({
+  id: joi.number().integer().required(),
+  name: joi.string().required()
+})
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -34,9 +40,13 @@ app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
 });
 
+// get planets
+
 app.get("/api/planets", (req, res) => {
   res.status(200).json(planets);
 });
+
+// get planet by id
 
 app.get("/api/planets/:id", (req, res) => {
   const { id } = req.params;
@@ -44,10 +54,36 @@ app.get("/api/planets/:id", (req, res) => {
   res.status(200).json(planet);
 });
 
+// post a planet
+
 app.post("/api/planets", (req, res) => {
   const { id, name } = req.body;
   const newPlanet = { id, name };
-  planets = [...planets, newPlanet];
+  const validatedNewPlanet = planetSchema.validate(newPlanet);
 
-  res.status(200).json({ msg: "The planet was created." });
+  if (validatedNewPlanet.error) {
+    return res.status(400).json({ msg: validatedNewPlanet.error.details[0].message });
+  } else {
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "The planet was created." });
+  }
 });
+
+// update a planet by id
+
+app.put("./api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map(p => p.id === Number(id) ? ({ ...p, name}) : p);
+
+  console.log(planets);
+
+  res.status(200).json({ msg: "The planet was updated"})
+})
+
+app.delete("./api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter(p => p.id !== Number(id));
+
+  res.status(200).json({ msg: "The planet was deleted"});
+})
